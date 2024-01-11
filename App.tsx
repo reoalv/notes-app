d23b7118453cbe100d4c6e241d4cc7da45243c95
reoalvarich <reoalvarich2609@gmail.com>
2024-01-11 18:27:31 +0700

    test ios & setup splashscreen ios

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Screen from './src/Screen';
-import {Platform, UIManager} from 'react-native';
+import {Platform, SafeAreaView, UIManager} from 'react-native';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {persistor, store} from './src/Redux/Store';
+import Colors from './src/Utils/Color';
 
 if (Platform.OS === 'android') {
   if (UIManager.setLayoutAnimationEnabledExperimental) {
@@ -15,7 +16,10 @@ const App = () => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <Screen />
+        <SafeAreaView
+          style={{flex: 1, backgroundColor: Colors.brandColor.softGreen}}>
+          <Screen />
+        </SafeAreaView>
       </PersistGate>
     </Provider>
   );
